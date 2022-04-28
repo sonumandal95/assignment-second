893f61c39fd9939acb7138be962fd476e13f90b5
sonumandal95 <55188699+sonumandal95@users.noreply.github.com>
2022-04-28 15:51:15 +0530

    picking algo
    read csv file
    convert it's data to json
    apply picking algo
    write result in json file

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('fast-csv');
+
+async function pickFriuts(path) {
+  let pickingFruits = { 'Future2': [], 'Future5': [], 'Future8': [] };
+  fs.createReadStream(path)
+    .pipe(parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', row => {
+      if (row['Future2'] > 70) pickingFruits['Future2'].push(row);
+      else if (row['Future5'] >= 70) pickingFruits['Future5'].push(row);
+      else pickingFruits['Future8'].push(row);
+    })
+    .on('end', function () {
+      console.log(pickingFruits)
+      fs.createWriteStream('pickingFruits.json').write(JSON.stringify(pickingFruits));
+    });
+}
+
+pickFriuts(path.resolve('./assets/ORRIR01C-test.csv'));
---
+++ readme.md
@@ -0,0 +1,8 @@
+To run the project
+1. install dependent package with below command
+    `npm install`
+2. run using below command
+    `node index.js`
+
+    it will create a file 'pickingFruits.json' which will have the data
+    like, list of fruits picked for Future2, Future5 and Future8
