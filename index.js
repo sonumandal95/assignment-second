8a9ab9f5eb58c92825aa1803a49bd3d363ffb2f3
sonumandal95 <55188699+sonumandal95@users.noreply.github.com>
2022-05-09 15:55:23 +0530

    avg fruit size on picking

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ async function pickFriuts(path) {
     .on('data', row => {
       let numberOfFuture = 0;
       let sumOfFruitsSize = 0;
+      // average fruit size
       for (key in row) {
         if (key.toString().startsWith('Future')) {
           numberOfFuture++;
@@ -17,9 +18,31 @@ async function pickFriuts(path) {
         }
       }
       row['AvgFruitSize'] = parseFloat(sumOfFruitsSize/numberOfFuture).toString();
+
       if (row['Future2'] > 70) pickingFruits['Future2'].push(row);
       else if (row['Future5'] >= 70) pickingFruits['Future5'].push(row);
       else pickingFruits['Future8'].push(row);
+
+      // average fruit size on picking
+      numberOfFuture = 0;
+      sumOfFruitsSize = 0;
+      for (key in row) {
+        if (key.toString().startsWith('Future')) {
+          numberOfFuture++;
+          sumOfFruitsSize += parseFloat(row[key]);
+        }
+        if (key === 'Future2' && row['Future2'] > 70) {
+          row['AvgFruitSizeOnPicking'] = parseFloat(sumOfFruitsSize/numberOfFuture).toString();
+          break;
+        }
+        else if (key === 'Future5' && row['Future2'] >= 70) {
+          row['AvgFruitSizeOnPicking'] = parseFloat(sumOfFruitsSize/numberOfFuture).toString();
+          break;
+        }
+        else if (key === 'Future9') {
+          row['AvgFruitSizeOnPicking'] = parseFloat(sumOfFruitsSize/numberOfFuture).toString();
+        }
+      }
     })
     .on('end', function () {
       console.log(pickingFruits)
